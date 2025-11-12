2010eb77c63244751d4f0e5f09763bd3bde94dea
SaulBgitgrub <saulbrito342@gmail.com>
2025-11-12 15:08:00 +0000

    operators

--- week 13  csp/ 1 — Comparison Operators in Python.py
+++ week 13  csp/ 1 — Comparison Operators in Python.py
@@ -12,24 +12,55 @@
 
 a = 3
 b = 4
-
-print(a == b)   # False
-print(a != b)   # True
-print(a > b)    # False
-print(a < b)    # True
-print(a >= b)   # False
-print(a <= b)   # True
+print(a) #output 3
+print(b) #output 4
+print(a == b)   # False. Checks equality
+print(a != b)   # True. Checks if it is not equal to another value
+print(a > b)    # False. Checks if greater than another value
+print(a < b)    # True. Checks if less than another value.
+print(a >= b)   # False. Checks if greater than or equal to.
+print(a <= b)   # True. Checks for less than or equal to.
 
 
 #predict the output of the following comparisons:
-10 > 5
-7 == 2 * 3 + 1
-8 != 8
-4 <= 2 + 2
+10 > 5 #Output true
+7 == 2 * 3 + 1 #Outpuit true
+8 != 8 #Output false
+4 <= 2 + 2 #Output True
 
 # Write 3 examples that result in True and 3 that result in False.
+10 > 6 #Output true
+8 == 2 * 3 + 2 #Output true
+9 != 2 #Output true
+
+5 > 6 #Output false
+10 != 10 #Output false
+5 == 8 - 1 #Output false
 
 # Create a simple grade-checking condition:
+score = int(input("What is your score? "))
+#Make this program for all grading spectrums
+# IF the score is between 90 - 100 .. you got an A
+# If the score is between 80 - 89... you got a B
+# IF the score is between 70 - 79 .. you got a C
+# IF the score is between 60 - 69 .. you got a D
+# IF the score is < 60 .. you got an F
+if score >= 90 and score <= 100:
+    print("You got an A")
+elif score >= 80 and score < 90:
+    print("You got an B")
+elif score >= 70 and score < 80:
+    print("You got an C")
+elif score >= 60 and score < 70:
+    print("You got an D")
+elif score < 60:
+    print("You got an F")
+if score >= 60:
+    print("You passed the test")
+else:
+    print("You didn't pass")
 
+#Ask for password
+password = input("What is your password ")
 # practice problem :
 # where a student must check if their score is greater than or equal to 60 to pass a test.# The password must be at least 8 characters long and contain at least one digit.password = "mypassword1"
