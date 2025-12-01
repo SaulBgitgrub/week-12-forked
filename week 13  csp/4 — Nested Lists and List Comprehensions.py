81a05e5ca54279d0e2d035748ed43d15be375d22
SaulBgitgrub <saulbrito342@gmail.com>
2025-12-01 14:30:57 +0000

    Nested lists

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -4,7 +4,24 @@
 # Key Notes:
 
 # A list can contain other lists.
+fruits =     ["apple", "orange", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats =      ["chicken", "fish", "turkey"]
 
+groceries = [fruits, vegetables, meats]
+
+fruits[0]
+print(groceries)
+# print(groceries)[0][1]
+# print(groceries)[1][2]
+# print(groceries)[2][2]
+
+for collection in groceries:
+    for food in collection:
+        print(food, end=" ") #, end=" "
+    # print(collection)
+    print()
+print("_")
 # List comprehensions provide a concise way to create lists.
 
 # Examples:Objective:
@@ -13,6 +30,14 @@
 # Key Notes:
 
 # A list can contain other lists.
+num_pad = ((1, 2, 3),
+           (4, 5, 6),
+           (7, 8, 9),
+           ("*", 0, "#"))
+for row in num_pad:
+    for num in row:
+        print(num, end=" ") #, end=" "
+    print()
 
 # List comprehensions provide a concise way to create lists.
 
